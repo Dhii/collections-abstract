20aa03f60eb33144d3a6caca35b04ab09523623a
Anton Ukhanev <xedin.unknown@gmail.com>
2016-10-14 19:14:05 +0200

    Renamed class for consistency

--- src/AbstractCallbackCollection.php
+++ src/AbstractCallbackCollection.php
@@ -2,22 +2,45 @@
 
 namespace Dhii\Collection;
 
+use Dhii\Stats;
+
 /**
  * Common functionality for callback collections.
  *
- * Ready to be extended and instantiated, with minimal or no modifications.
- *
  * @since [*next-version*]
  */
-abstract class AbstractCallbackCollection extends AbstractCallbackCollectionBase implements CallbackIterableInterface
+abstract class AbstractCallbackCollection extends Stats\AbstractAggregatableCollection
 {
     /**
-     * {@inheritdoc}
+     * Retrieve a callback instance that will apply a callback to each item in the list.
+     *
+     * See {@see CallbackIterableInterface::each()} for details about the callback.
      *
      * @since [*next-version*]
+     *
+     * @param callable           $callback The callback to apply to each item in the list. Default: itself.
+     * @param array|\Traversable $items    The list of items to iterate over.
+     *
+     * @return CallbackIteratorInterface The callback iterator that will iterate over the list.
      */
-    public function each($callback)
+    protected function _each($callback, $items = null)
     {
-        return $this->_each($callback, $this);
+        if (is_null($items)) {
+            $items = $this;
+        }
+
+        return $this->_createCallbackIterator($callback, $items);
     }
+
+    /**
+     * Create a new instance of callback iterator for the specified callback and item list.
+     *
+     * @since [*next-version*]
+     *
+     * @param callable           $callback The callback for the iterator to apply to each item.
+     * @param array|\Traversable $items    The list of items for the iterator to iterate over.
+     *
+     * @return CallbackIterator The new callback iterator.
+     */
+    abstract protected function _createCallbackIterator($callback, $items);
 }
